8763e5ef4e11f80fae9e675bd10d5c0206b4277f
cshoaib05 <choudharyshoaib05@gmail.com>
2018-09-18 15:54:14 +0530

    Laravel

---
+++ PHP/web.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>A webpage</title>
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
+</head>
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.html"> HOME</a></li>
+				<li><a href="resume.html">ABOUT ME</a></li>
+				<li><a href="blog.html">BLOG</a></li>
+				<li><a href="gallery.html">GALLERY</a></li>
+				<li><a href="contact.html">CONTACT</a></li>
+			</ul>
+		</nav>
+
+	</header>
+
+	<main>
+		<div>
+			<figure class="photo">
+				<img src="IMG/dffa3371bf3bd129701856cef1d37ed1.jpg"/>
+			</figure>
+		</div>
+		<h2>BLENDER</h2>
+		<p class="ioo">
+		This is the webpage for my Professional Detail.
+		</p>
+		<div>
+			<p>
+				<ul>
+					<li>Computer Engineering.</li>
+					<li>PUBG</li>
+					<li>KURLA</li>
+				</ul>
+			</p>
+			<a href="resume.html">Know More About Me!!!</a>		
+		</div>
+		<div>
+			<a href="contact.html">Contact Me!!!</a>
+		</div>
+		<div>
+			<a href="blog.html">My Blog</a>
+		</div>
+		<div>
+			<a href="gallery.html">Gallery</a>
+		</div>
+	</main>
+
+	<footer>
+		<div>
+			&copy; ALL right reserved.
+		</div>
+	</footer>
+	<script type="text/javascript" src="JS/app.js">
+	</script>
+
+</body>
+</html>
